66123d3c96088e011f2880ce6b7d8c82aea4ba84
serious <fikurimufc@gmail.com>
2016-11-25 11:18:19 +0700

    adding composer and layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,7 @@
         {{HTML::script('assets/material-design/js/material.min.js')}}
         <!-- {{HTML::script('assets/plugin/bootstrap-wysihtml5/ripples.min.js')}} -->
         {{HTML::script('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}
+        {{HTML::script('https://www.google.com/recaptcha/api.js' async defer)}}
     </head>
 	<body>
 		@yield('content')
